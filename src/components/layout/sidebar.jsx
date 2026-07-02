e52bcb464448804859ea0c8897d5026ed8b9b127
YUANQTIE <yuanpanlilio@gmail.com>
2026-07-02 22:54:23 +0800

    chore: added sidebar to project structure editor

--- src/components/layout/sidebar.jsx
+++ src/components/layout/sidebar.jsx
@@ -14,7 +14,7 @@ export default function SidebarLayout({ children }) {
   const [collapsed, setCollapsed] = useState(false);
 
   return (
-    <div className="flex h-screen w-screen bg-gray-100">
+    <div className="flex min-h-screen w-full bg-gray-100">
       {/* Sidebar */}
       <div
         className={`
